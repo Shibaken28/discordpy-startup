f936e75d0d162e7a6cd26673a77716e2c806e2fd
Shibaken28 <60318970+Shibaken28@users.noreply.github.com>
2020-05-24 22:40:12 +0900

    Update bunkai.py
    素数ですを言うように

--- bunkai.py
+++ bunkai.py
@@ -91,7 +91,7 @@ async def on_message(message):
 
         output=f'{message.author.mention}\n'
 
-        if(len(prime)!=1):
+        if(len(prime)!=2):
             output+=str(sn)+"="
             k=1
             for i in prime:
