5932154ba7367a02fd056926020ecf493fbf9a3b
Shibaken28 <60318970+Shibaken28@users.noreply.github.com>
2020-05-24 22:35:59 +0900

    Update bunkai.py
    ごめんなさい

--- bunkai.py
+++ bunkai.py
@@ -20,7 +20,7 @@ async def on_message(message):
     if sentence.startswith("!help"):
         await message.channel.send("!bunkai N  Nを素因数分解した結果を表示させる")
         await message.channel.send("!neko  にゃーんと鳴かせる")
-    if 'TINTIN' in message.content:
+    if ('TINTIN' in message.content) or ('おっぱい' in message.content) or ('ちんちん' in message.content) or ('ero' in message.content)or ('Ero' in message.content)or ('エロ' in message.content):
         file_img = discord.File("partyParrot.gif")
         await message.channel.send(file=file_img)
     if 'おはよう' in message.content:
